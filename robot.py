a51c0f8eea0627af844998ddb2edf8aef2a8ae18
Andrea Bartolucci <computerman0224@yahoo.it>
2016-01-28 22:18:12 +0100

    Aggiunte altre direzioni ai sensori e direzione del robot

--- robot.py
+++ robot.py
@@ -4,15 +4,29 @@ import line
 
 class Sensor(Enum):
     FORWARD = 1
+    LEFT = 2
+    RIGHT = 3
+    BACK = 4
+    def offset_angle(self):
+        if self.name == "FORWARD":
+            return 0
+        if self.name == "LEFT":
+            return math.pi/2
+        if self.name == "RIGHT":
+            return -math.pi/2
+        if self.name == "BACK":
+            return math.pi
 
 class Robot():
     def __init__(self, mappa, position):
         self.mappa = mappa
         self.position = position
+        self.angle = math.pi/2
 
     def sense(self, sensor):
         #https://web.archive.org/web/20120612145751/http://local.wasp.uwa.edu.au/~pbourke/geometry/lineline2d/
-        v_sensor = [self.position[0], self.position[1]+40]
+        #Crea un sensore che parte dal robot e punta all'angolo self.angle
+        v_sensor = [self.position[0]+math.cos(self.angle+sensor.offset_angle()), self.position[1]+math.sin(self.angle+sensor.offset_angle())]
         distances = []
         for linea in self.mappa.linee:
             denominatore = (linea[1][1]-linea[0][1])*(v_sensor[0]-self.position[0])-(linea[1][0]-linea[0][0])*(v_sensor[1]-self.position[1])
@@ -25,6 +39,7 @@ class Robot():
                 #Le linee sono parallele, passiamo alla prossima
                 continue
             if ua > 0 and 0 <= ub <= 1:
+                #Calcolo della distanza
                 x = self.position[0]+ua*(v_sensor[0]-self.position[0])
                 y = self.position[1]+ua*(v_sensor[1]-self.position[1])
                 dx = x - self.position[0]
@@ -32,3 +47,6 @@ class Robot():
                 dist_squared =  dx**2 + dy**2
                 distances.append(dist_squared)
         return math.sqrt(min(distances))
+
+    def turn(self, angle):
+        self.angle += angle
--- test/test_robot.py
+++ test/test_robot.py
@@ -1,6 +1,7 @@
 import unittest
 import robot
 import mappa
+import math
 
 class TestRobot(unittest.TestCase):
     def test_robot_creation(self):
@@ -12,7 +13,36 @@ class TestRobot(unittest.TestCase):
         m.load("maps/mappaSensori.map")
         r = robot.Robot(m, (15,15))
         dist_r = r.sense(robot.Sensor.FORWARD)
-        self.assertAlmostEqual(dist_r, 15.0, 0.1)
+        self.assertAlmostEqual(dist_r, 15, delta=0.1)
+
+    def test_turn_sensor(self):
+        m = mappa.Mappa()
+        m.load("maps/mappaSensori.map")
+        r = robot.Robot(m, (15,15))
+        r.turn(-(45/180)*math.pi)
+        dist_r = r.sense(robot.Sensor.FORWARD)
+        self.assertAlmostEqual(dist_r, 15.55, delta=0.1)
+
+    def test_sensor_left(self):
+        m = mappa.Mappa()
+        m.load("maps/mappaSensori.map")
+        r = robot.Robot(m, (15,15))
+        dist_r = r.sense(robot.Sensor.LEFT)
+        self.assertAlmostEqual(dist_r, 13.0, delta=0.1)
+
+    def test_sensor_right(self):
+        m = mappa.Mappa()
+        m.load("maps/mappaSensori.map")
+        r = robot.Robot(m, (15,15))
+        dist_r = r.sense(robot.Sensor.RIGHT)
+        self.assertAlmostEqual(dist_r, 11.0, delta=0.1)
+
+    def test_sensor_back(self):
+        m = mappa.Mappa()
+        m.load("maps/mappaSensori.map")
+        r = robot.Robot(m, (15,15))
+        dist_r = r.sense(robot.Sensor.BACK)
+        self.assertAlmostEqual(dist_r, 12.0, delta=0.1)
 
 if __name__ == '__main__':
     unittest.main()
